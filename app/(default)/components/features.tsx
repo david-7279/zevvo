a596ea276caa0da6095f8b19feeb462b567d3c7c
David Vieira <davidsoares7279@gmail.com>
2025-06-19 18:29:27 +0100

    feat: update features information

--- app/(default)/components/features.tsx
+++ app/(default)/components/features.tsx
@@ -1,67 +1,75 @@
-import React from "react"
+import React from "react";
+import {
+  BarChartIcon,
+  BoxesIcon,
+  FileTextIcon,
+  PackageIcon,
+  UsersIcon,
+} from "lucide-react";
 
 import Heading from "@/app/(default)/components/heading";
-import { BoxesIcon, UserIcon } from "lucide-react"
+
+const featuresData = [
+  {
+    title: "Gestão de Clientes Simplificada",
+    description:
+      "Registe e organize seus clientes com filtros, etiquetas e pesquisa avançada. Acompanhe interações e impulsione suas vendas com facilidade.",
+    icon: UsersIcon,
+  },
+  {
+    title: "Faturas Automáticas",
+    description:
+      "Crie, personalize e envie faturas em PDF automaticamente. Receba alertas de vencimento e mantenha o controlo dos pagamentos.",
+    icon: FileTextIcon,
+  },
+  {
+    title: "Relatórios Interativos",
+    description:
+      "Acesse relatórios de vendas com gráficos dinâmicos e exporte dados em Excel. Tome decisões informadas para crescer seu negócio.",
+    icon: BarChartIcon,
+  },
+  {
+    title: "Gestão de Produtos Simplificada",
+    description:
+      "Registe e organize seus produtos com filtros, etiquetas e pesquisa avançada. Acompanhe interações e impulsione suas vendas com facilidade.",
+    icon: PackageIcon,
+  },
+];
 
 const Features = () => {
   return (
     <div className="w-full py-20 lg:py-40">
       <div className="container mx-auto">
-        <div className="flex flex-col gap-10 text-center justify-center items-center">
+        <div className="flex flex-col gap-10 justify-center items-center">
           <Heading
             icon={BoxesIcon}
             title="Funcionalidades"
             heading="Construído para satisfazer as suas necessidades!"
-            paragraph="Descrição para as funcionalidades do sistema"
+            paragraph="Descubra como nossa plataforma simplifica a gestão do seu negócio local com ferramentas poderosas e fáceis de usar."
           />
-
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
-              <UserIcon className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
-                </p>
+            {featuresData.map((feature, index) => (
+              <div
+                key={index}
+                className={`bg-muted rounded-md p-6 flex justify-between flex-col ${index === 0 || index === 3
+                    ? "h-full lg:col-span-2 aspect-square lg:aspect-auto"
+                    : "aspect-square"
+                  }`}
+              >
+                <feature.icon className="w-8 h-8 stroke-1" />
+                <div className="flex flex-col">
+                  <h3 className="text-xl tracking-tight">{feature.title}</h3>
+                  <p className="text-muted-foreground max-w-xs text-base">
+                    {feature.description}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="bg-muted rounded-md  aspect-square p-6 flex justify-between flex-col">
-              <UserIcon className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
-                </p>
-              </div>
-            </div>
-
-            <div className="bg-muted rounded-md aspect-square p-6 flex justify-between flex-col">
-              <UserIcon className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
-                </p>
-              </div>
-            </div>
-            <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
-              <UserIcon className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
