a97db7685968eb80ef32b56d25301e5b1e43c910
David Vieira <davidsoares7279@gmail.com>
2025-06-19 15:22:25 +0100

    feat: update nav bar

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Navbar from "@/components/layout/navbar";
+import Navbar from "@/components/layout/(default)/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
