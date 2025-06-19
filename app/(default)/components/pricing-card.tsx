efac2602f54ce7b7f63a548e32841d202db9fb73
David Vieira <davidsoares7279@gmail.com>
2025-06-19 18:11:25 +0100

    feat: add pricing card component

---
+++ app/(default)/components/pricing-card.tsx
@@ -0,0 +1,77 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { BorderBeam } from "@/components/magicui/border-beam";
+import { Path } from "@/lib/paths";
+import { CheckIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+interface Feature {
+  title: string;
+  description: string;
+}
+
+interface Props {
+  index: number;
+  title: string;
+  description: string;
+  price: number;
+  features: Feature[];
+}
+
+const PricingCard: React.FC<Props> = ({ index, title, description, price, features }) => {
+  return (
+    <Card key={index} className="relative overflow-hidden w-full rounded-md">
+      <CardHeader>
+        <CardTitle>
+          <span className="flex flex-row gap-4 items-center font-normal">{title}</span>
+        </CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-8 justify-start">
+          <p className="flex flex-row  items-center gap-2 text-xl">
+            <span className="text-4xl">${price}</span>
+            <span className="text-sm text-muted-foreground">
+              {" "}
+              / mês
+            </span>
+          </p>
+          <div className="flex flex-col gap-4 justify-start">
+            {features.map((feature, idx) => (
+              <div className="flex flex-row gap-4" key={idx}>
+                <CheckIcon className="w-4 h-4 mt-2 text-primary" />
+                <div className="flex flex-col">
+                  <p>{feature.title}</p>
+                  <p className="text-muted-foreground text-sm">{feature.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <Link href={Path.root} className="w-full">
+            {index === 0 || index === 2 ? (
+              <Button variant="outline" className="w-full">
+                <span className="whitespace-pre-wrap text-center text-sm leading-none tracking-tight">
+                  Começar
+                </span>
+              </Button>
+            ) : null}
+
+            {index === 1 ? (
+              <Button variant="default" className="w-full">
+                <span className="whitespace-pre-wrap text-center text-sm leading-none tracking-tight">
+                  Começar
+                </span>
+              </Button>
+            ) : null}
+          </Link>
+        </div>
+      </CardContent>
+
+      {index === 1 ? (<BorderBeam duration={8} size={100} />) : null}
+    </Card>
+  )
+}
+
+export default PricingCard;
--- app/(default)/components/pricing.tsx
+++ app/(default)/components/pricing.tsx
@@ -9,13 +9,47 @@ import { Button } from "@/components/ui/button";
 import { Check, CircleDollarSignIcon, MoveRight, PhoneCall } from "lucide-react";
 
 import Heading from "@/app/(default)/components/heading";
+import PricingCard from "./pricing-card";
+
+const pricingInformation = [
+  {
+    index: 0,
+    title: "Startup",
+    description: "",
+    price: 10,
+    features: [
+      { title: "Feature A", description: "Descrição A" },
+      { title: "Feature B", description: "Descrição B" }
+    ]
+  },
+  {
+    index: 1,
+    title: "Standard",
+    description: "",
+    price: 20,
+    features: [
+      { title: "Feature C", description: "Descrição C" },
+      { title: "Feature D", description: "Descrição D" }
+    ]
+  },
+  {
+    index: 2,
+    title: "Premium",
+    description: "",
+    price: 30,
+    features: [
+      { title: "Feature E", description: "Descrição E" },
+      { title: "Feature F", description: "Descrição F" }
+    ]
+  },
+]
 
 const Pricing = () => {
   return (
     <div className="w-full py-20 lg:py-40">
       <div className="container mx-auto">
         <div className="flex text-center justify-center items-center gap-4 flex-col">
-          <Heading 
+          <Heading
             icon={CircleDollarSignIcon}
             title="Preços"
             heading="Preços que fazem sentido!"
@@ -23,174 +57,16 @@ const Pricing = () => {
           />
 
           <div className="grid pt-20 text-left grid-cols-1 lg:grid-cols-3 w-full gap-8">
-            <Card className="w-full rounded-md">
-              <CardHeader>
-                <CardTitle>
-                  <span className="flex flex-row gap-4 items-center font-normal">
-                    Startup
-                  </span>
-                </CardTitle>
-                <CardDescription>
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever for everyone and everywhere.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-8 justify-start">
-                  <p className="flex flex-row  items-center gap-2 text-xl">
-                    <span className="text-4xl">$40</span>
-                    <span className="text-sm text-muted-foreground">
-                      {" "}
-                      / month
-                    </span>
-                  </p>
-                  <div className="flex flex-col gap-4 justify-start">
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <Button variant="outline" className="gap-4">
-                    Sign up today <MoveRight className="w-4 h-4" />
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-            <Card className="w-full shadow-2xl rounded-md">
-              <CardHeader>
-                <CardTitle>
-                  <span className="flex flex-row gap-4 items-center font-normal">
-                    Growth
-                  </span>
-                </CardTitle>
-                <CardDescription>
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever for everyone and everywhere.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-8 justify-start">
-                  <p className="flex flex-row  items-center gap-2 text-xl">
-                    <span className="text-4xl">$40</span>
-                    <span className="text-sm text-muted-foreground">
-                      {" "}
-                      / month
-                    </span>
-                  </p>
-                  <div className="flex flex-col gap-4 justify-start">
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <Button className="gap-4">
-                    Sign up today <MoveRight className="w-4 h-4" />
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-            <Card className="w-full rounded-md">
-              <CardHeader>
-                <CardTitle>
-                  <span className="flex flex-row gap-4 items-center font-normal">
-                    Enterprise
-                  </span>
-                </CardTitle>
-                <CardDescription>
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever for everyone and everywhere.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-8 justify-start">
-                  <p className="flex flex-row  items-center gap-2 text-xl">
-                    <span className="text-4xl">$40</span>
-                    <span className="text-sm text-muted-foreground">
-                      {" "}
-                      / month
-                    </span>
-                  </p>
-                  <div className="flex flex-col gap-4 justify-start">
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-row gap-4">
-                      <Check className="w-4 h-4 mt-2 text-primary" />
-                      <div className="flex flex-col">
-                        <p>Fast and reliable</p>
-                        <p className="text-muted-foreground text-sm">
-                          We&apos;ve made it fast and reliable.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <Button variant="outline" className="gap-4">
-                    Book a meeting <PhoneCall className="w-4 h-4" />
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
+            {pricingInformation.map((item, index) => (
+              <PricingCard
+                key={index}
+                index={item.index}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                features={item.features}
+              />
+            ))}
           </div>
         </div>
       </div>
