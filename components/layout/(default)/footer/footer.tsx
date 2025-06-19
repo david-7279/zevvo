2b7d326d5e3fcaa7dfdd1e91e8b2c454eb51f80d
David Vieira <davidsoares7279@gmail.com>
2025-06-19 22:15:28 +0100

    feat: update footer

--- components/layout/(default)/footer/footer.tsx
+++ components/layout/(default)/footer/footer.tsx
@@ -1,7 +1,8 @@
-import { Path } from '@/lib/paths'
-import React from 'react'
-import MapLinks from './map-links'
-import Link from 'next/link'
+import React from "react"
+import Link from "next/link"
+import { Path } from "@/lib/paths"
+
+import MapLinks from "@/components/layout/(default)/footer/map-links"
 
 const footerLinks = [
   { label: "Funcionalidades", link: `${Path.root}#features` },
@@ -14,8 +15,10 @@ const Footer = () => {
 
   return (
     <div className="mx-auto mt-2 max-w-6xl px-6 lg:px-12 space-y-5 mb-5">
-      <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4 border-muted-foreground border-b">
-        <p>@{year} Business Hub, Todos os direitos reservados</p>
+      <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4 border-muted-foreground/20 border-b">
+        <Link href={Path.root} className="flex flex-row gap-2 items-center">
+          <p className="text-base text-foreground/70">Business Hub</p>
+        </Link>
 
         <div className="flex flex-row gap-4">
           {footerLinks.map((item) => (
@@ -28,18 +31,19 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-1">
-        <p className="text-muted-foreground text-sm">Desenvolvido por</p>
-        <Link href={"https://david7279.vercel.app/"} target="_blank" className="group">
-          <p className="relative text-sm text-foreground transition-all duration-500
+      <div className="flex justify-between items-center gap-1">
+        <p className="text-sm text-muted-foreground">@{year}, Todos os direitos reservados</p>
+
+        <Link href={Path.root} className="group flex flex-row gap-1 items-center">
+          <p className="relative text-sm text-muted-foreground transition-all duration-500
             after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-foreground after:transition-all after:duration-500
             group-hover:after:w-full"
           >
-            David Vieira
+            Termos e Condições
           </p>
         </Link>
       </div>
-    </div>
+    </div >
   )
 }
 
