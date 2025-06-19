59f342b95903f15cb28fe380defb62941a16857d
David Vieira <davidsoares7279@gmail.com>
2025-06-19 15:51:52 +0100

    feat: add nav wrapper

--- components/layout/(default)/header.tsx
+++ components/layout/(default)/header.tsx
@@ -1,33 +1,19 @@
-"use client"
-import React, { useEffect, useState } from "react";
+import React from "react";
 
+import NavWrapper from "@/components/layout/(default)/nav-wrapper";
 import Logo from "@/components/layout/(default)/logo";
 import Menu from "@/components/layout/(default)/menu";
 import Account from "@/components/layout/(default)/account";
-import { cn } from "@/lib/utils"
 
 const Header = () => {
-  const [isScrolled, setIsScrolled] = React.useState(false)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50)
-    }
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [])
 
   return (
     <header>
-      <nav className="fixed w-full top-5 px-2">
-        <div className={cn("mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12", isScrolled && "bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5")}>
-          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
-            <Logo />
-            <Menu />
-            <Account />
-          </div>
-        </div>
-      </nav>
+      <NavWrapper>
+        <Logo />
+        <Menu />
+        <Account />
+      </NavWrapper>
     </header>
   );
 };
---
+++ components/layout/(default)/nav-wrapper.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import React, { useState, useEffect, ReactNode } from "react"
+import { cn } from "@/lib/utils";
+
+interface Props {
+  classname?: string;
+  children: ReactNode
+}
+
+const NavWrapper: React.FC<Props> = ({ classname, children }) => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <nav className={`fixed w-full top-5 px-2${classname}`}>
+      <div className={cn("mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12", isScrolled && "bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5")}>
+        <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+          {children}
+        </div>
+      </div>
+      /</nav>
+  )
+}
+
+export default NavWrapper
