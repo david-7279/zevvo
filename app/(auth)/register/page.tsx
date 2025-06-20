ffd41e36f13ea49c58ed78de928680c970d29565
David Vieira <davidsoares7279@gmail.com>
2025-06-20 12:39:24 +0100

    feat: update auth responsive layout

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -9,39 +9,52 @@ import { Card, CardContent } from '@/components/ui/card'
 import { PackageIcon } from "lucide-react";
 
 import SiderContent from "@/app/(auth)/components/sider-content";
+import { Button } from "@/components/ui/button";
 
 const Register = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="w-full h-full grid lg:grid-cols-2 p-4">
-        <div className="max-w-xs m-auto w-full flex flex-col space-y-5">
-          <div>
-            <PackageIcon />
-            <p className="mt-2 text-xl font-bold tracking-tight">Registar conta no Bussiness Hub</p>
-            <small className="text-sm text-muted-foreground leading-none font-medium">Bem-vindo! Cria uma conta para continuar</small>
+      <div className="w-full h-full grid lg:grid-cols-2">
+
+        {/* LEFT PANEL */}
+        <div className=" w-full flex flex-col">
+          <div className="text-left p-10 items-end hidden lg:block">
+            <Link href={Path.root}>
+              <Button variant="link" className="rounded-2xl">Zevvo</Button>
+            </Link>
           </div>
 
-          <Card className="relative w-[350px] overflow-hidden">
-            <CardContent>
-              <RegisterForm />
-            </CardContent>
-            <BorderBeam duration={8} size={100} />
-          </Card>
+          <div className="max-w-sm m-auto w-full flex flex-col items-center space-y-5 px-4 mt-20">
+            <div className="items-start text-left w-full">
+              <PackageIcon />
+              <p className="mt-2 text-xl font-bold tracking-tight">Registar conta no Zevvo</p>
+              <small className="text-sm text-muted-foreground leading-none font-medium">Bem-vindo! Cria uma conta para continuar</small>
+            </div>
 
-          <div className="mt-5 space-y-5">
-            <div className="flex justify-center items-center gap-1">
-              <p className="text-muted-foreground text-sm">Já tem uma conta?</p>
-              <Link href={Path.login} className="group">
-                <p className="relative text-sm text-foreground transition-all duration-500
+            <Card className="relative w-full overflow-hidden">
+              <CardContent>
+                <RegisterForm />
+              </CardContent>
+              <BorderBeam duration={8} size={100} />
+            </Card>
+
+            <div className="mt-5">
+              <div className="flex justify-center items-center gap-1">
+                <p className="text-muted-foreground text-sm">Já tem uma conta?</p>
+                <Link href={Path.login} className="group">
+                  <p className="relative text-sm text-foreground transition-all duration-500
                   after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-foreground after:transition-all after:duration-500
                   group-hover:after:w-full"
-                >
-                  Iniciar Sessão
-                </p>
-              </Link>
+                  >
+                    Iniciar Sessão
+                  </p>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
+        
+        {/* RIGHT PANEL */}
         <SiderContent />
       </div>
     </div>
