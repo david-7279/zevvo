73f12b987ba471729c2bfc17fe1e024ff9966dd0
David Vieira <davidsoares7279@gmail.com>
2025-06-24 16:00:54 +0100

    feat: remove api auth route

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth"
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [    
-  ],
-}
-
-export default NextAuth(authOptions)
