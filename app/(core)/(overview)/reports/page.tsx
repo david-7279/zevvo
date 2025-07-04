daec2909e5d868b651a0a246fdd3318f51512efe
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-04 19:29:38 +0100

    feat: update report header

--- app/(core)/(overview)/reports/page.tsx
+++ app/(core)/(overview)/reports/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react"
+
+import CoreWrapper from "@/app/(core)/_components/core-wrapper"
+
 
 const Reports = () => {
   return (
-    <div>Reports</div>
+    <CoreWrapper>
+      {/* Report Title */}
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Relatórios</h3>
+
+      {/* Header Cards Row */}
+      <div className="flex flex-row flex-wrap justify-between gap-5">
+
+      </div >
+
+      {/* Data Table */}
+    </CoreWrapper>
   )
 }
 
