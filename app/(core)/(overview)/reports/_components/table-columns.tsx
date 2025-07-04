30c6564b305a3f37806dcd29cc5a4c5d20ae5b35
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-04 20:08:45 +0100

    feat: update table report columns component

--- app/(core)/(overview)/reports/_components/table-columns.tsx
+++ app/(core)/(overview)/reports/_components/table-columns.tsx
@@ -4,9 +4,12 @@ import DataTableColumnRow from "@/app/(core)/(overview)/reports/_components/tabl
 
 interface ReportData {
   clientName: string
+  productName: string
+  price: number
+  timeStamp: string
 }
 
-export const columnsSubject: ColumnDef<ReportData>[] = [
+export const columnsReport: ColumnDef<ReportData>[] = [
   {
     accessorKey: "clientName",
     id: "Client",
@@ -14,4 +17,41 @@ export const columnsSubject: ColumnDef<ReportData>[] = [
     accessorFn: (row) => row.clientName,
     cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
   },
-] 
+  {
+    accessorKey: "productName",
+    id: "Product",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Produto" tooltip="Nome do produto" />,
+    accessorFn: (row) => row.productName,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
+  },
+  {
+    accessorKey: "price",
+    id: "Price",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Preço" tooltip="Preço do produto" />,
+    accessorFn: (row) => row.price,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
+  },
+  {
+    accessorKey: "timeStamp",
+    id: "Time Stamp",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Data" tooltip="Data da compra" />,
+    accessorFn: (row) => row.timeStamp,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
+  },
+  // action for delete and edit
+  {
+    accessorKey: "action",
+    id: "Action",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Ação" tooltip="Ação do produto" />,
+    // accessorFn: (row) => row.action,
+    cell: ({ row }) => {
+      // const book = row.original.book;
+      return (
+        <div className="flex gap-2">
+          {/* <ActionUpdateBook book={book} onBookUpdated={onBookUpdated} />
+          <ActionDeleteBook book={book} onBookDeleted={onBookDeleted} /> */}
+        </div>
+      );
+    },
+  }
+]
