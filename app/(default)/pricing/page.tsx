3f6cb9ddd7a782f4c1e2984d1be49ed06182bec7
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-19 21:26:27 +0100

    fix: button component lib path

---
+++ app/(default)/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pricing = () => {
+  return (
+    <section className="@container mx-auto max-w-5xl lg:px-0 mt-40 px-4">
+      Pricing Page
+    </section>
+  )
+}
+
+export default Pricing
