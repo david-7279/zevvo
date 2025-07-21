1f012540f469071e16646b82b39e54739f859d6b
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-21 15:22:30 +0100

    feat: update pricing card information

--- app/(default)/pricing/page.tsx
+++ app/(default)/pricing/page.tsx
@@ -1,12 +1,43 @@
 import React from 'react'
-import CoreWrapper from '@/app/(core)/_components/core-wrapper'
-import Heading from '../_components/heading'
-import { EuroIcon } from 'lucide-react'
+import { Axis3DIcon, BarChartIcon, ChartAreaIcon, ClipboardListIcon, Columns3CogIcon, FileTextIcon, PackageIcon } from 'lucide-react'
+import { Path } from '@/lib/paths';
+import { BentoCard, BentoGrid } from '@/components/magicui/bento-grid';
+
+const pricingHeader = [
+  {
+    Icon: ClipboardListIcon,
+    name: "Startup",
+    description: "Ideal para pequenos negócios que estão a começar e querem organizar clientes e vendas de forma simples.",
+    href: Path.dashboard,
+    cta: "Começar Agora",
+    className: "col-span-3 lg:col-span-1",
+    background: <img className="absolute -right-20 -top-20 opacity-60" alt="Gestão de Clientes" />,
+  },
+
+  {
+    Icon: ChartAreaIcon,
+    name: "Standard",
+    description: "Perfeito para negócios em crescimento que precisam de mais controlo e automação.",
+    href: Path.dashboard,
+    cta: "Começar Agora",
+    className: "col-span-3 lg:col-span-1",
+    background: <img className="absolute -right-20 -top-20 opacity-60" alt="Faturas Automáticas" />,
+  },
+  {
+    Icon: Columns3CogIcon,
+    name: "Premium",
+    description: "A solução completa para negócios que querem maximizar eficiência e escalar operações.",
+    href: Path.dashboard,
+    cta: "Começar Agora",
+    className: "col-span-3 lg:col-span-1",
+    background: <img className="absolute -right-20 -top-20 opacity-60" alt="Relatórios Interativos" />,
+  },
+];
 
 const Pricing = () => {
   return (
     <section className="scroll-py-16 py-16 md:scroll-py-32 md:py-32 mt-5">
-      <div className="mx-auto max-w-5xl px-6">
+      <div className="mx-auto max-w-5xl px-6 space-y-16">
 
         <div className="space-y-5 flex justify-center items-center text-center">
           <div className="flex gap-2 flex-col">
@@ -20,6 +51,12 @@ const Pricing = () => {
         </div>
 
         <div className="grid gap-y-12 px-4 lg:[grid-template-columns:1fr_auto]">
+
+          <BentoGrid>
+            {pricingHeader.map((pricing) => (
+              <BentoCard key={pricing.name} {...pricing} />
+            ))}
+          </BentoGrid>
 
 
         </div>
