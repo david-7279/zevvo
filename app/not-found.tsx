8fb51009ed6b5093bb9fa1a084d586d84f6207bb
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-21 20:51:27 +0100

    feat: add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Path } from '@/lib/paths'
+import { Button } from '@/components/ui/button'
+import Error404 from "@/public/404.gif"
+import CoreWrapper from './(core)/_components/core-wrapper'
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import Header from "@/components/layout/(default)/header/header";
+import Footer from "@/components/layout/(default)/footer/footer";
+
+import "../app/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const NotFound = () => {
+  return (
+    <html lang="pt" className={`${geistSans.variable} ${geistMono.variable} scroll-smooth`} suppressHydrationWarning>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div>
+          <Image
+            src={Error404.src}
+            alt='Error 404'
+            width={400}
+            height={400}
+          />
+        </div>
+
+        <div className="space-y-5 mt-15 items-center text-center text-pretty">
+          <h3 className="text-4xl font-semibold">Página não encontrada</h3>
+          <div className="flex flex-col gap-1">
+            <p className="text-foreground text-base">A página que procura não existe ou foi movida.</p>
+            <p className="text-foreground text-base">Mas de certeza que vai gostar do nosso trabalho.</p>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <Link href={Path.root}>
+            <Button variant="default">
+              Ir para a página inicial
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </html>
+  )
+}
+
+export default NotFound
