0c0a972afca8ef8e569187da10ef77e27c09835c
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-28 19:38:07 +0100

    feat: edit payment page

--- app/(core)/(features)/payments/edit/[id]/page.tsx
+++ app/(core)/(features)/payments/edit/[id]/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import CoreWrapper from '@/app/(core)/_components/core-wrapper'
+import EditPaymentForm from "@/app/(core)/(features)/_components/forms/edit-payment"
 
 const EditPayment = () => {
   return (
-    <div>EditPayment</div>
+    <CoreWrapper>
+      <EditPaymentForm />
+    </CoreWrapper>
   )
 }
 
