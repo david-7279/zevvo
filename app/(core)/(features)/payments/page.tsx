a4e0a22db84d71aa1ad34a546cf032dda613e2eb
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-28 17:09:25 +0100

    feat: add payments page structure

--- app/(core)/(features)/payments/page.tsx
+++ app/(core)/(features)/payments/page.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from "react"
+import CoreWrapper from "@/app/(core)/_components/core-wrapper"
+import FeatureHeaderCardComponent from "../_components/header-card"
+import { CheckCircleIcon, ClockIcon, EuroIcon, TriangleAlertIcon } from "lucide-react"
+import ClientsContent from "@/app/(core)/(features)/clients/clients-content"
+import PaymentsContent from "@/app/(core)/(features)/payments/payments-content"
+
+const customData = [
+  {
+    title: "Total de Pagamentos",
+    value: "€45,230",
+    description: "Total de pagamentos deste mês",
+    percentage: "+2.1 %",
+    trend: "up" as const,
+    icon: EuroIcon,
+  },
+  {
+    title: "Recebidos",
+    value: "€38,450",
+    description: "Total de pagamentos deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: CheckCircleIcon,
+  },
+  {
+    title: "Pendentes",
+    value: "€5,680",
+    description: "Total de clientes ativos deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: ClockIcon,
+  },
+  {
+    title: "Falhados",
+    value: "€1,100",
+    description: "Total de pagamentos deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: TriangleAlertIcon,
+  },
+]
 
 const Payments = () => {
   return (
-    <div>Payments</div>
+    <CoreWrapper>
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Pagamentos</h3>
+      <div className="space-y-16">
+        {/* Header Cards Row */}
+        <div className="flex flex-row flex-wrap gap-5">
+          <FeatureHeaderCardComponent data={customData} />
+        </div >
+
+        {/* CONTENT */}
+        <PaymentsContent />
+      </div>
+    </CoreWrapper>
   )
 }
 
---
+++ app/(core)/(features)/payments/payments-content.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentsContent = () => {
+  return (
+    <div>PaymentsContent</div>
+  )
+}
+
+export default PaymentsContent
