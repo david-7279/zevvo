dc49cfc839c039ae90b58a1e250ec66ce25dfc73
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-28 10:33:10 +0100

    feat: add clients header structure

--- app/(core)/(features)/clients/page.tsx
+++ app/(core)/(features)/clients/page.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from "react"
+import CoreWrapper from "@/app/(core)/_components/core-wrapper"
+import FeatureHeaderCardComponent from "../_components/header-card"
+import { DollarSignIcon, PackageIcon, TriangleAlertIcon, UsersIcon } from "lucide-react"
+
+
+const customData = [
+  {
+    title: "Total de Clientes",
+    value: "122",
+    description: "Total de clientes deste mês",
+    percentage: "+8.2%",
+    trend: "up" as const,
+    icon: PackageIcon,
+  },
+  {
+    title: "Novos Clientes",
+    value: "30",
+    description: "Total de novos clientes deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: DollarSignIcon,
+  },
+  {
+    title: "Clientes Ativos",
+    value: "3",
+    description: "Total de clientes ativos deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: TriangleAlertIcon,
+  },
+  {
+    title: "Pagamentos deste mês",
+    value: "€4,125",
+    description: "Total de pagamentos deste mês",
+    percentage: "-2.1%",
+    trend: "down" as const,
+    icon: UsersIcon,
+  },
+]
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <CoreWrapper>
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Clientes</h3>
+      <div className="space-y-16">
+        {/* Header Cards Row */}
+        <div className="flex flex-row flex-wrap gap-5">
+          <FeatureHeaderCardComponent data={customData} />
+        </div >
+
+        {/* CONTENT */}
+
+      </div>
+    </CoreWrapper>
   )
 }
 
