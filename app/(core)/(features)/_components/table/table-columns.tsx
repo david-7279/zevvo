3ceec8e0d3db055bd7b09b45afc8cf09bc6ea51e
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-28 17:20:21 +0100

    feat: add payments columns

--- app/(core)/(features)/_components/table/table-columns.tsx
+++ app/(core)/(features)/_components/table/table-columns.tsx
@@ -30,6 +30,15 @@ interface ClientsData {
   phone: string
 }
 
+interface PaymentsData {
+  id: number
+  client: string
+  product: string
+  price: number
+  date: string
+  paymentStatus: string
+}
+
 export const columnsProduct: ColumnDef<ProductData>[] = [
   {
     accessorKey: "id",
@@ -151,6 +160,86 @@ export const columnsClients: ColumnDef<ClientsData>[] = [
     header: ({ column }) => <DataTableColumnHeader column={column} title="Número de telemóvel" tooltip="Número de telemóvel do cliente" />,
     accessorFn: (row) => row.phone,
     cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
+  },
+  {
+    accessorKey: "action",
+    id: "Action",
+    header: ({ column }) => <DataTableColumnHeaderNoSort column={column} className="flex justify-center items-center w-full h-full" title="Ações" tooltip="Ações do cliente" />,
+    cell: (row) => {
+      return (
+        <div className="flex justify-center items-center w-full h-full ">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0 cursor-pointer">
+                <MoreHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <ClientRowAction id={row.row.original.id} MenuItem={DropdownMenuItem} />
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      );
+    }
+  }
+]
+
+export const columnsPayments: ColumnDef<PaymentsData>[] = [
+  {
+    accessorKey: "id",
+    id: "Identifier",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="ID" tooltip="Identificador único do cliente" />,
+    accessorFn: row => row.id,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="font-medium" />,
+  },
+  {
+    accessorKey: "client",
+    id: "Client",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Cliente" tooltip="Nome do cliente" />,
+    accessorFn: (row) => row.client,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="font-medium" />,
+  },
+  {
+    accessorKey: "product",
+    id: "Product",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Produto/Servico" tooltip="Produto ou servico" />,
+    accessorFn: (row) => row.product,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="font-medium" />,
+  },
+  {
+    accessorKey: "date",
+    id: "date",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Data/Hora" tooltip="Data e hora do pagamento" />,
+    accessorFn: (row) => row.date,
+    cell: ({ getValue }) => <DataTableColumnRow getValue={String(getValue())} className="" />,
+  },
+  {
+    accessorKey: 'paymentStatus',
+    id: "Status",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Estado" tooltip="Estado do pagamento" />,
+    accessorFn: (row) => row.paymentStatus,
+    cell: ({ row }) => {
+      const status = row.getValue('Status') as string
+
+      const styles = {
+        'Recebido':
+          'bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 [a&]:hover:bg-green-600/5 dark:[a&]:hover:bg-green-400/5',
+        'Pendente':
+          'bg-destructive/10 [a&]:hover:bg-destructive/5 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive',
+        'Falhado':
+          'bg-amber-600/10 text-amber-600 focus-visible:ring-amber-600/20 dark:bg-amber-400/10 dark:text-amber-400 dark:focus-visible:ring-amber-400/40 [a&]:hover:bg-amber-600/5 dark:[a&]:hover:bg-amber-400/5'
+      }[status]
+
+      return (
+        <Badge className={(cn('rounded-full border-none focus-visible:outline-none'), styles)}>
+          {row.getValue('Status')}
+        </Badge>
+      )
+    },
+    enableSorting: true,
+    meta: {
+      filterVariant: 'select'
+    }
   },
   {
     accessorKey: "action",
