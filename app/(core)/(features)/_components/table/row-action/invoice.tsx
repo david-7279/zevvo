d41a17bbd8adb8f6e4ba7438cf67f4d75697fd12
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-29 13:13:22 +0100

    feat: update row action

---
+++ app/(core)/(features)/_components/table/row-action/invoice.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { EyeIcon, EditIcon, Trash2Icon, XIcon, DownloadIcon } from "lucide-react";
+
+export function InvoiceRowAction({ id, MenuItem }: { id: string | number, MenuItem: React.ElementType }) {
+  return (
+    <>
+      <MenuItem className="gap-2" asChild>
+        <Link href={`/clients/${id}`}>
+          <EyeIcon className="h-4 w-4" />
+          Ver Fatura
+        </Link>
+      </MenuItem>
+      <MenuItem className="gap-2" asChild>
+        <Link href={`/clients/edit/${id}`}>
+          <DownloadIcon className="h-4 w-4 text-muted-foreground" />
+          Descarregar Fatura
+        </Link>
+      </MenuItem>
+      <MenuItem className="gap-2" asChild>
+        <Link href={`/clients/edit/${id}`}>
+          <EditIcon className="h-4 w-4 text-muted-foreground" />
+          Editar
+        </Link>
+      </MenuItem>
+      <MenuItem className="gap-2 text-red-600">
+        <Trash2Icon className="h-4 w-4 text-red-600" />
+        Eliminar
+      </MenuItem>
+      <MenuItem className="gap-2">
+        <XIcon className="h-4 w-4" />
+        Cancelar
+      </MenuItem>
+    </>
+  );
+}
---
+++ app/(core)/(features)/_components/table/row-action/product.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { EyeIcon, EditIcon, Trash2Icon, XIcon } from "lucide-react";
+
+export function ProductRowActions({ id, MenuItem }: { id: string | number, MenuItem: React.ElementType }) {
+  return (
+    <>
+      <MenuItem className="gap-2" asChild>
+        <Link href={`/products/${id}`}>
+          <EyeIcon className="h-4 w-4" />
+          Ver Produto
+        </Link>
+      </MenuItem>
+      <MenuItem className="gap-2" asChild>
+        <Link href={`/products/edit/${id}`}>
+          <EditIcon className="h-4 w-4 text-muted-foreground" />
+          Editar
+        </Link>
+      </MenuItem>
+      <MenuItem className="gap-2 text-red-600">
+        <Trash2Icon className="h-4 w-4 text-red-600" />
+        Eliminar
+      </MenuItem>
+      <MenuItem className="gap-2">
+        <XIcon className="h-4 w-4" />
+        Cancelar
+      </MenuItem>
+    </>
+  );
+}
