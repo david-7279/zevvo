dda2848f74d2edc57837f75be9d4918a62b4021f
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-29 14:21:14 +0100

    feat: supplier id page

--- app/(core)/(features)/suppliers/[id]/page.tsx
+++ app/(core)/(features)/suppliers/[id]/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
 
-const Supplier = () => {
+import { useState } from "react"
+import FeaturesAside from "@/app/(core)/(features)/_components/siderbar/aside";
+import FeaturesContent from "@/app/(core)/(features)/_components/siderbar/content";
+import CoreWrapper from "@/app/(core)/_components/core-wrapper";
+import { InfoIcon } from "lucide-react";
+
+const supplierData = [
+  {
+    id: "supplier-details",
+    title: "Detalhes",
+    icon: InfoIcon,
+  },
+]
+
+const SupplierPage = () => {
+  const [activeId, setActiveId] = useState(supplierData[0].id)
+
   return (
-    <div>Supplier</div>
+    <CoreWrapper>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex flex-col lg:flex-row gap-8">
+          <FeaturesAside items={supplierData} activeId={activeId} onItemClick={setActiveId} type="do fornecedor" />
+
+          <main className="flex-1 min-w-0">
+            <FeaturesContent activeId={activeId} />
+          </main>
+        </div>
+      </div>
+    </CoreWrapper>
   )
 }
 
-export default Supplier
+export default SupplierPage
