5ef8d0061a1fee97eb647c7d12092e357c57571e
David Soares Vieira <davidsoares7279@gmail.com>
2025-07-29 13:17:27 +0100

    feat: add invoice filter

---
+++ app/(core)/(features)/invoices/_components/invoice-filter.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoiceFilter = () => {
+  return (
+    <div>InvoiceFilter</div>
+  )
+}
+
+export default InvoiceFilter
--- app/(core)/(features)/invoices/invoices-content.tsx
+++ app/(core)/(features)/invoices/invoices-content.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from "react"
-import ProductFilters from "../products/product-filters";
+import ProductFilters from "@/app/(core)/(features)/invoices/_components/invoice-filter";
 import InvoiceTable from "@/app/(core)/(features)/invoices/_components/invoice-table";
 import { columnsInvoices } from "@/app/(core)/(features)/_components/table/table-columns";
 import { InvoiceRowAction } from "@/app/(core)/(features)/_components/table/row-action/invoice";
